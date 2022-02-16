97d379a6b45091aa3a1fe76cff68fd1fa6f120ea
andreshgkenility <andres.heredia@kenility.com>
2022-02-16 18:54:55 -0400

    adding build sh for later

---
+++ cd $FCI_BUILD_DIR.sh
@@ -0,0 +1,17 @@
+cd $FCI_BUILD_DIR
+
+pushd android
+./gradlew app:assembleAndroidTest
+./gradlew app:assembleDebug -Ptarget="$FCI_BUILD_DIR/integration_test/app_test.dart"
+popd
+
+echo $GCLOUD_KEY_FILE | base64 --decode > ./gcloud_key_file.json
+
+gcloud auth activate-service-account --key-file=gcloud_key_file.json
+
+gcloud --quiet config set project integration-test-tutoria-39a51
+
+gcloud firebase test android run \
+--type instrumentation \
+--app build/app/outputs/apk/debug/app-debug.apk \
+--test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
